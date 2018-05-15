b820fff065596144a1e109fbec4464fb2686e10b
Rom Vic <technoyguy@gmail.com>
2018-05-15 15:00:04 -0700

    added Routes

---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export const SIGN_UP ='/signup'
+export const SIGN_IN ='/signin'
+export const LANDING ='/'
+export const HOME ='/home'
+export const ACOUNT ='/account'
+export const PASSWORD_FORGET ='/pw_forget'
+
