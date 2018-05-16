255cffd72b601b03d09a0c79623a4d2011e78f35
Rom Vic <technoyguy@gmail.com>
2018-05-16 12:38:35 -0700

    React Session handling implementation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import AuthUserContext from './AuthUserContext'
 import * as routes from '../constants/routes'
 import SignOutButton from './SignOut';
 
-const Navigation = ({authUser}) => 
-        <div>
-            {authUser? <NavigationAuth/>:<NavigationNonAuth/>}
-        </div>
-  
-const NavigationAuth=()=>
+const Navigation = () =>
+    <AuthUserContext.Consumer> 
+            {authUser=>authUser? <NavigationAuth/>:<NavigationNonAuth/>}
+    </AuthUserContext.Consumer>
 
-        <ul>
-              <li><Link to={routes.LANDING}> Landing</Link></li>
+const NavigationAuth=()=>
+       <ul>
+            <li><Link to={routes.LANDING}> Landing</Link></li>
             <li><Link to={routes.HOME}> Home</Link></li>
             <li><Link to={routes.ACOUNT}> Account</Link></li>
             <li><SignOutButton/></li>
         </ul>
 
 const NavigationNonAuth=()=>
-  <ul>
-      <li><Link to={routes.LANDING}> Landing</Link></li>
-      <li><Link to={routes.SIGN_IN}> Sign In</Link></li>
-
-     
-  </ul>
-
-
+    <ul>
+        <li><Link to={routes.LANDING}> Landing</Link></li>
+        <li><Link to={routes.SIGN_IN}> Sign In</Link></li>
+    </ul>
 export default Navigation
